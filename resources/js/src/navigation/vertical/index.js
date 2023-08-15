8ae8a86f60cc8655089e9bcac30eeb0dffbd959d
masadi <masadi.com@gmail.com>
2023-08-16 00:47:06 +0700

    update

--- resources/js/src/navigation/vertical/index.js
+++ resources/js/src/navigation/vertical/index.js
@@ -43,7 +43,7 @@ export default [...dashboard, ...appsAndPages, ...uiElements, ...formAndTable, .
 import beranda from './1_beranda'
 import referensi from './2_referensi'
 import presensi from './3_presensi'
-import jadwal_mengajar from './4_jadwal-mengajar'
+//import jadwal_mengajar from './4_jadwal-mengajar'
 import penilaian from './5_penilaian'
 import wali_kelas from './6_wali-kelas'
 import monitoring from './7_monitoring'
@@ -55,7 +55,7 @@ export default [
     ...beranda, 
     ...referensi,
     ...presensi,
-    ...jadwal_mengajar,
+    //...jadwal_mengajar,
     ...penilaian,
     ...wali_kelas,
     ...monitoring,
