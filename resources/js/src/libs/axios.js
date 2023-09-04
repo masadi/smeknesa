c726f3d169da4bf1fc22e97093cb67055ffd206b
masadi <masadi.com@gmail.com>
2023-09-04 22:55:35 +0700

    update

--- resources/js/src/libs/axios.js
+++ resources/js/src/libs/axios.js
@@ -16,19 +16,44 @@ axiosIns.interceptors.request.use(function (config) {
 }, function (error) {
   return Promise.reject(error);
 });
-axiosIns.interceptors.response.use(
+/*axiosIns.interceptors.response.use(
   response => response,
   error => {
     if (error.response && 419 === error.response.status) {
       window.location.reload()
-    } else if (error.response && 401 === error.response.status) {
+    } else if (error.response && 401 === error.response.status && !error.response.errors) {
       localStorage.removeItem('userData')
       localStorage.removeItem('accessToken')
       localStorage.removeItem('userAbilities')
       this.$router.push('/login')
     } else {
+      return Promise.reject(error);
+      //return error.response
+    }
+});*/
+axiosIns.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  if(error.response.data){
+    if(error.response.data.errors){
       return error.response
     }
+  }
+  if (error.response && 419 === error.response.status) {
+    window.location.reload()
+  } else if (error.response && 401 === error.response.status) {
+    localStorage.removeItem('userData')
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('userAbilities')
+    this.$router.push('/login')
+  } else {
+    return Promise.reject(error);
+    //return error.response
+  }
 });
 Vue.prototype.$http = axiosIns
 
--- resources/js/src/views/pages/Login.vue
+++ resources/js/src/views/pages/Login.vue
@@ -198,12 +198,26 @@ export default {
                 })
               })
             } else {
-              //console.log(response.data);
               this.$refs.loginForm.setErrors(response.data.errors)
             }
-          }).catch(error => {
+          }).catch(function (error) {
             console.log('bawah');
-            console.log(error);
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
             //this.$refs.loginForm.setErrors(error.response.data.error)
           })
         }
