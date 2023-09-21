b4dcea2be66fc2d98235764d8c2c55ac03e32066
masadi <masadi.com@gmail.com>
2023-09-21 22:16:19 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\KirimEmail;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command(KirimEmail::class)->dailyAt('17:00');
         // $schedule->command('inspire')->hourly();
     }
 
