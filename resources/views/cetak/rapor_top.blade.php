a9de12688623c5857ab8d3c9ef12d38e68f13d39
masadi <masadi.com@gmail.com>
2023-10-16 17:38:11 +0700

    update

--- resources/views/cetak/rapor_top.blade.php
+++ resources/views/cetak/rapor_top.blade.php
@@ -4,7 +4,7 @@
 <br>
 <br>
 <br>
-<img src="{{($get_siswa->rombongan_belajar->sekolah && $get_siswa->rombongan_belajar->sekolah->logo_sekolah) ? public_path('./storage'.config('erapor.storage').'/images/'.$get_siswa->rombongan_belajar->sekolah->logo_sekolah) : public_path('./images/tutwuri.png')}}" style="max-height: 200px" />
+<img src="{{($get_siswa->rombongan_belajar->sekolah && $get_siswa->rombongan_belajar->sekolah->logo_sekolah) ? asset('storage/images/'.$get_siswa->rombongan_belajar->sekolah->logo_sekolah) : asset('img/logo/tutwuri.png')}}" style="max-height: 200px" />
 <br>
 <br>
 <br>
